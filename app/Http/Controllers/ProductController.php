1c77b3778d0e514e4141e7cfc7cde4fc356f2d16
MohdHaseenAhamad <happyhassen0786@gmail.com>
2023-07-18 20:00:12 +0530

    image upload issue solve

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,15 +8,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 
-class ProductController extends Controller
-{
+class ProductController extends Controller {
     /**
      * Display a listing of the resource.
-     *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
+    public function index() {
         $results = Product::all();
         return response()->json([
             'status' => 200,
@@ -27,26 +24,22 @@ class ProductController extends Controller
 
     /**
      * Show the form for creating a new resource.
-     *
      * @return \Illuminate\Http\Response
      */
-    public function create()
-    {
+    public function create() {
 
     }
 
     /**
      * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
+    public function store(Request $request) {
 //        die;
         $validator = validator::make($request->all(), [
             'name' => ['required', 'min:2', 'max:100'],
-            'image' => ['required','image','mimes:jpg,png,jpeg,gif,svg','max:2048'],
+            'image' => ['required', 'image', 'mimes:jpg,png,jpeg,gif,svg', 'max:2048'],
             'pieces' => ['required'],
             'price' => ['required'],
             'description' => ['required', 'min:2', 'max:500'],
@@ -61,81 +54,77 @@ class ProductController extends Controller
                 'message' => $validator->messages(),
             ], 400);
         } else {
-//            $imageName = time().'.'.$request->image->extension();
-            $imageName = $request->file('image')->getRealPath();
-            Cloudder::upload($imageName, null);
-            // Public Folder
-//            $request->image->move(public_path('images'), $imageName);
+            $width = 250;
+            $height = 250;
+
+            $image_name = $request->file('image')->getRealPath();
+            Cloudder::upload($image_name, null, array(
+                "folder" => "foodatize_products_images",  "overwrite" => FALSE,
+                "resource_type" => "image", "responsive" => TRUE, "transformation" => array("quality" => "70", "width" => "250", "height" => "250", "crop" => "scale")
+            ));
+            $image_url = Cloudder::show(Cloudder::getPublicId(), ["width" => $width, "height" => $height, "crop" => "scale", "quality" => 70, "secure" => "true"]);
+
             $pro = new Product();
             $pro->name = $request->name;
-            $pro->image = $imageName;
+            $pro->image = $image_url;
             $pro->pieces = $request->pieces;
             $pro->price = $request->price;
             $pro->description = $request->description;
             $pro->save();
-if($pro->save())
-{
-    return response()->json([
-        'status' => 200,
-        'message' => "item add Successfully",
+            if ($pro->save()) {
+                return response()->json([
+                    'status' => 200,
+                    'message' => "item add Successfully",
 
-    ], 200);
-}
+                ], 200);
+            }
 
         }
     }
 
     /**
      * Display the specified resource.
-     *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
+    public function show($id) {
         //
     }
 
     /**
      * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
-    {
+    public function edit($id) {
         //
     }
 
     /**
      * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
-    {
+    public function update(Request $request, $id) {
         //
     }
 
     /**
      * Remove the specified resource from storage.
-     *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
-    {
+    public function destroy($id) {
         //
     }
-    public function filter(Request $request, Product $pro)
-    {
+
+    public function filter(Request $request, Product $pro) {
         $pro = $pro->newQuery();
 
         // Search for a user based on their name.
         if ($request->has('name')) {
-            $pro->where('name','like', '%' .$request->input('name'). '%')->orWhere('description','like','%' .$request->input('name'). '%');
+            $pro->where('name', 'like', '%' . $request->input('name') . '%')->orWhere('description', 'like', '%' . $request->input('name') . '%');
         }
         return response()->json([
             'status' => 200,
